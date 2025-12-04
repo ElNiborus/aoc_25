6a060ff30f0c05c46d59d1404d333efbdb905048
Robin / Yoga Ubuntu <klubarob@fit.cvut.cz>
2025-12-04 15:15:07 +0100

    Formatting

--- 01/task2.py
+++ 01/task2.py
@@ -18,20 +18,13 @@ with open(file_name, 'r') as file:
 for value in value_list:
     spins = abs(value) // total_pins
     rest = abs(value) % total_pins
-
-    if spins > 0:
-         print(f'Fullspins: {spins}')
-         code += spins
+    code += spins
 
     if ((curr+value) % total_pins) == 0:
-        print('Stopped at 0')
         code +=1
     
     if (curr!=0) and (((value < 0) and (rest > curr)) or ((value > 0) and (rest > (total_pins-curr)))):
-        print(f'Pass by zero with spin')
         code +=1
 
     curr += value
     curr = curr % total_pins
-    
-print(code)
